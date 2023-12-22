dc9471599fb095790de04ce124c4d3d22edad361
Zafar <icom@MACKBOOK>
2023-12-22 08:15:12 +0300

    CODE RUB: Refactor Code

--- assistant_just_in_case.core/Controllers/HomeController.cs
+++ assistant_just_in_case.core/Controllers/HomeController.cs
@@ -3,6 +3,8 @@
 // Fast assistant everywhere
 //===========================
 
+using assistant_just_in_case.core.Models.TelegramUserMessages;
+using assistant_just_in_case.Services.Orchestrations.Telegrams;
 using Microsoft.AspNetCore.Mvc;
 
 namespace assistant_just_in_case.core.Controllers
@@ -11,8 +13,18 @@ namespace assistant_just_in_case.core.Controllers
     [Route("[controller]")]
     public class HomeController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<string> AssistantIsWorking() =>
-            Ok("Assistant is working...");
+        private readonly ITelegramUserOrchestrationService userOrchestrationService;
+
+        public HomeController(ITelegramUserOrchestrationService userOrchestrationService)
+        {
+            this.userOrchestrationService = userOrchestrationService;
+        }
+
+        [HttpPost]
+        public void AssistantIsWorking()
+        {
+            this.userOrchestrationService.ListenTelegramUserMessage();
+        }
+
     }
 }
